e02ecfb34fa9028d97b6688e18059a0b225563de
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-09-12 15:45:43 +0200

    update animals table

--- queries.sql
+++ queries.sql
@@ -16,3 +16,39 @@ SELECT * FROM animals
 WHERE name != 'Gabumon';
 SELECT * FROM animals
 WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+SAVEPOINT BEFORE;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK BEFORE;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals SET species ='digimon' WHERE name::text LIKE '%mon';
+UPDATE animals SET species ='pokemon' WHERE name::text NOT LIKE '%mon';
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+SAVEPOINT BEFOREDEL;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK TO BEFOREDEL;
+SELECT * FROM animals;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT changeWeight;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK changeWeight;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT (*) FROM animals;
+SELECT COUNT (name) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT COUNT(escape_attempts), neutered FROM animals GROUP BY neutered;
+SELECT MIN(weight_kg), MAX (weight_kg), species FROM animals GROUP BY species;
+SELECT AVG(escape_attempts), species FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2022-12-31' GROUP BY species;
